5733c2da9d69b013a1dbc12c1d83948b77af3903
Scalco7 <fscalco7@gmail.com>
2024-02-18 10:34:49 -0300

    status-component

---
+++ RpgWorld/src/heroes/components/data.tsx
@@ -0,0 +1,121 @@
+import { StyleSheet, View, Text, Image } from "react-native";
+
+export interface IHeroData {
+    color: string;
+    name: string;
+    title: string;
+    strength: number;
+    health: number;
+    shield: number;
+    agility: number;
+    magicPower: number;
+    range: number;
+    stamina: number;
+};
+
+const iconStrength = require("../../../assets/icons/dumbbell.png");
+const iconHealth = require("../../../assets/icons/heart.png");
+const iconShield = require("../../../assets/icons/shield.png");
+const iconAgility = require("../../../assets/icons/agility.png");
+const iconMagicPower = require("../../../assets/icons/potion.png");
+const iconRange = require("../../../assets/icons/range.png");
+const iconStamina = require("../../../assets/icons/stamina.png");
+
+function DataComponent(props: any): React.JSX.Element {
+    const heroData: IHeroData = props.heroData;
+
+    return (
+        <View style={[styles.boxAll, { backgroundColor: heroData.color }]}>
+            <View>
+                <Text style={[styles.text, styles.nameText]}>{heroData.name}</Text>
+                <Text style={[styles.text, styles.titleText]}>{heroData.title}</Text>
+            </View>
+            <View style={styles.boxAllStatus}>
+                <View>
+                    <View style={styles.boxStatus}>
+                        <Image source={iconStrength} style={styles.imgIcon}></Image>
+                        <Text style={[styles.text, styles.statusText]}>Força: {heroData.strength}</Text>
+                    </View>
+                    <View style={styles.boxStatus}>
+                        <Image source={iconHealth} style={styles.imgIcon}></Image>
+                        <Text style={[styles.text, styles.statusText]}>Vida: {heroData.health}</Text>
+                    </View>
+                    <View style={styles.boxStatus}>
+                        <Image source={iconShield} style={styles.imgIcon}></Image>
+                        <Text style={[styles.text, styles.statusText]}>Defesa: {heroData.shield}</Text>
+                    </View>
+                    <View style={styles.boxStatus}>
+                        <Image source={iconAgility} style={styles.imgIcon}></Image>
+                        <Text style={[styles.text, styles.statusText]}>Agilidade: {heroData.agility}</Text>
+                    </View>
+                </View>
+                <View>
+                    <View style={styles.boxStatus}>
+                        <Image source={iconMagicPower} style={styles.imgIcon}></Image>
+                        <Text style={[styles.text, styles.statusText]}>Magia: {heroData.magicPower}</Text>
+                    </View>
+                    <View style={styles.boxStatus}>
+                        <Image source={iconRange} style={styles.imgIcon}></Image>
+                        <Text style={[styles.text, styles.statusText]}>Alcance: {heroData.range}</Text>
+                    </View>
+                    <View style={styles.boxStatus}>
+                        <Image source={iconStamina} style={styles.imgIcon}></Image>
+                        <Text style={[styles.text, styles.statusText]}>Estamina: {heroData.stamina}</Text>
+                    </View>
+                </View>
+            </View>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    boxAll: {
+        width: '100%',
+        paddingHorizontal: 15,
+        paddingVertical: 20
+    },
+
+    boxAllStatus: {
+        flexDirection: 'row',
+        alignItems: 'flex-start',
+        justifyContent: 'center',
+        gap: 30
+    },
+
+    boxStatus: {
+        padding: 0,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        gap: 10
+    },
+
+    text: {
+        fontFamily: 'InknutAntiqua-SemiBold',
+        color: 'black',
+    },
+
+    nameText: {
+        lineHeight: 40,
+        fontSize: 20
+    },
+
+    titleText: {
+        lineHeight: 25,
+        fontSize: 13
+    },
+
+    statusText: {
+        lineHeight: 34,
+        fontSize: 15
+    },
+
+    imgIcon: {
+        alignItems: 'center',
+        objectFit: 'contain',
+        width: 20,
+        height: 20
+    }
+});
+
+export default DataComponent;
--- RpgWorld/src/heroes/screen/heroes.tsx
+++ RpgWorld/src/heroes/screen/heroes.tsx
@@ -1,7 +1,51 @@
 import { View, StyleSheet, Image } from "react-native";
 import GoBackButton from "../../components/go-back-button/button";
+import DataComponent from "../components/data";
+
+export interface IHero {
+    img: string;
+    color: string;
+    name: string;
+    title: string;
+    strength: number;
+    health: number;
+    shield: number;
+    agility: number;
+    magicPower: number;
+    range: number;
+    stamina: number;
+};
 
 const backgroundImage = require('../../../assets/background/heroes.png');
+
+const heroes: IHero[] = [
+    {
+        img: "",
+        color: "#DAD8CB",
+        name: "Sir ALdric",
+        title: "o cavalheiro errante",
+        strength: 90,
+        health: 70,
+        shield: 100,
+        agility: 70,
+        magicPower: 20,
+        range: 20,
+        stamina: 100
+    },
+    {
+        img: "",
+        color: "#6A8462",
+        name: "Kallarix",
+        title: "o lagarto das Sombras",
+        strength: 30,
+        health: 50,
+        shield: 45,
+        agility: 100,
+        magicPower: 70,
+        range: 30,
+        stamina: 50
+    },
+]
 
 function HeroesScreen(props: any): React.JSX.Element {
     return (
@@ -9,6 +53,9 @@ function HeroesScreen(props: any): React.JSX.Element {
             <Image source={backgroundImage} style={styles.imageBackground}></Image>
             <View style={styles.backButton}>
                 <GoBackButton navigation={props.navigation}></GoBackButton>
+            </View>
+            <View style={{ flex: 1, alignItems: 'flex-end', justifyContent: 'flex-end' }}>
+                <DataComponent heroData={heroes[0]}></DataComponent>
             </View>
         </View>
     );
